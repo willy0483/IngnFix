6dafb7c485b65532807a71583a196a92ea04c318
willy0483 <willyjensen251@gmail.com>
2025-02-14 10:09:10 +0100

    fix

--- src/components/gridContainer/gridContainer.Styled.js
+++ src/components/gridContainer/gridContainer.Styled.js
@@ -1,17 +1,6 @@
 import styled from "styled-components";
 
-export const GridItem = styled.div`
-  ${({ index }) =>
-    index >= 9 &&
-    index <= 1000 &&
-    `
-    figure {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-
-    }
-  `}
-`;
+export const GridItem = styled.div``;
 export const GridContainerStyled = styled.div`
   display: grid;
   grid-template-areas:
